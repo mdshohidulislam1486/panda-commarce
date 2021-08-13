55cb17ed85d0b51e866dbac8ffb5e502a5ad45b4
mdshohidulislam1486 <Shohidul Islam>
2021-08-14 02:08:37 +0600

    done for to night

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-document.getElementsByTagName('h4').style.color = "lightblue"
+const allh4Tag =document.getElementsByTagName("h4");
+for(const allh4 of allh4Tag){
+    allh4.style.color = "lightblue";
+}
+
+document.getElementById('tomato').style.backgroundColor ="tomato";
+
+const cardSlectors = document.querySelectorAll(".card")
+for(const cardSelector of cardSlectors){
+    cardSelector.style.borderRadius = "30px"
+}
+
+
+document.getElementById('add-console').addEventListener('click', function(){
+    console.log("Clikc the first by Supply 350 sho and check the console")
+})
+
+const allBuyButtons =document.querySelectorAll('button.btn-dark');
+for(const button of allBuyButtons){
+    button.addEventListener('click', function(event){
+        let targetDiv = event.target.parentElement;
+        targetDiv.removeChild(event.target)
+ })
+}
+
+document.getElementById('enable-text').addEventListener("keyup", function(event){
+const innnerText = event.target.value
+const btnDisable = document.getElementById("bnt-disable")
+if(innnerText == "email"){
+btnDisable.removeAttribute("disabled")
+}else{
+    btnDisable.setAttribute("disabled", true)
+}
+
+})
+document.getElementById('bnt-disable').addEventListener('click', function(){
+const palinText = document.getElementById('enable-text');
+palinText.value ='';
+})
